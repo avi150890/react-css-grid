e989f2c9d990665b4d88ba7afd8a711d4962e4e0
avi150890 <avinash15sharma@gmail.com>
2021-02-07 01:59:12 +0530

    version 1.0.1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Grid from './components/grid'
 
-const arrOfObj = [{value:1,color: '#6F98A8'},{value:2,color: '#2B8EAD'}, {value:3,color: '#2F454E'}, {value:4,color: '#2B8EAD'}, {value:5,color: '#2F454E'}, {value:6,color: '#BFBFBF'}, 
-{value:7,color:'#BFBFBF'}, {value:8,color: '#6F98A8'}, {value:9,color: '#2F454E'}]
-
 function App() {
+
+  const arrOfObj = [{ value: 1, color: '#6F98A8' },
+  { value: 2, color: '#2B8EAD' }, { value: 3, color: '#2F454E' },
+  { value: 4, color: '#2B8EAD' }, { value: 5, color: '#2F454E' },
+  { value: 6, color: '#BFBFBF' }, { value: 7, color: '#BFBFBF' },
+  { value: 8, color: '#6F98A8' }, { value: 9, color: '#2F454E' }]
+
   const [result, setResult] = useState(arrOfObj);
-  function shuffleArray(array) {
-    let i = array.length - 1;
-    for (; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    console.log("array",array);
-    setResult(array);
+
+  // useEffect(() => {
+  //   console.log("insideeffect", result);
+  // }, [result]);
+
+  console.log("on first load",result);
+
+  const sortedArray = (array)=> {
+    let sorted = array.sort((a,b) =>(a.value > b.value) ? 1 : ((b.value > a.value) ? -1 : 0));
+    console.log("sorted", sorted);
+    setResult(sorted);
+    console.log("result", result);
   }
+
+  const shuffledArray = (array)=> {
+    let shuffled = array.sort(() => Math.random() - 0.5);
+    console.log("shuffled", shuffled);
+    setResult(shuffled);
+    console.log("result", result);
+  }
+
   return (
     <div className="App">
+      <div className="gridButtonTop">
+        <button onClick={() => shuffledArray(result)} className="suffling">SHUFFLE</button>
+        <button onClick={() => sortedArray(result)} className="sorting">SORT</button>
+      </div>
       <Grid arrOfObj={result} />
-      <button onClick={()=>setResult(arrOfObj)} className="sorting">Sort</button>
-      <button  onClick={()=>shuffleArray(arrOfObj)} className="suffling">Suffle</button>
+      <div className="gridButtonBottom">
+        <button onClick={() => shuffledArray(result)} className="suffling">SHUFFLE</button>
+        <button onClick={() => sortedArray(result)} className="sorting">SORT</button>
+      </div>
     </div>
   );
 }
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export default function Grid (props) {
+  console.log("props",props);
   const cells = props.arrOfObj.map(obj => 
     <div key={obj.value} style={{ backgroundColor: obj.color}} 
     className="cell">{obj.value}</div>)
 
   return (
-    <div className="container">
+    <div key={props.arrOfObj[0].value} className="container">
       {cells}
     </div>
   )
